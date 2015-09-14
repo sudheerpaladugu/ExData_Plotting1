863d50bd2632c3c6e128a8dbb471f288f2163b1a
sudheerpaladugu <sudheerpaladugu@yahoo.com>
2015-09-13 19:13:23 -0500

    Plot3 file
    Plot R code file

---
+++ plot3.R
@@ -0,0 +1,15 @@
+source("loadData.R")
+png(filename = "plot3.png", 
+    width = 480, height = 480,
+    units = "px", bg = "transparent")
+plot(twoDayspwrConsData$DateTime, twoDayspwrConsData$Sub_metering_1, 
+     type = "l",
+     col = "black",
+     xlab = "", ylab = "Energy sub metering")
+lines(twoDayspwrConsData$DateTime, twoDayspwrConsData$Sub_metering_2, col = "red")
+lines(twoDayspwrConsData$DateTime, twoDayspwrConsData$Sub_metering_3, col = "blue")
+legend("topright", 
+       col = c("black", "red", "blue"),
+       c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       lwd = 1)
+dev.off()
